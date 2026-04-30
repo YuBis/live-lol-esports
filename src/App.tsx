8d07540c592be64c800c475f6d2740d8ca6d6506
cshoon1996 <cshoon1996@gmail.com>
2026-04-30 15:30:37 +0900

    Remove: betting advertisement banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import { EventsSchedule } from "./components/Schedule/EventsSchedule";
 import { Navbar } from "./components/Navbar/Navbar";
 import { useTheme } from './theme/ThemeContext'
 import React from "react";
-import ThunderPickBanner from './thunderpick_960x140.png';
-
 function App() {
     const { theme } = useTheme();
 
@@ -23,9 +21,6 @@ function App() {
                         <Route path="/live/:gameid" component={Match} />
                         <Redirect to="/" />
                     </Switch>
-                    <a className='thunderPickBanner' href="https://go.thunder.partners/visit/?bta=37411&nci=5627&campaign=WELCOME" target="_top">
-                        <img src={ThunderPickBanner} width="100%" height="auto" loading="lazy" />
-                    </a>
                 </div>
                 <Footer />
             </div>
